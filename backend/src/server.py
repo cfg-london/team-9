fa4a79b0132a95e8f77ed63dd567bfd7e073c58e
Cosmin Pascaru <cosmin.pascaru2@gmail.com>
2017-11-18 01:22:59 +0200

    Add homepage graph API page and graph class

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
+from backend.src.graph import Graph
+
 app = Flask(__name__)
 api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument("workflow", type=dict)
 
+
 @app.route('/')
 def index():
     return 'Index!'
+
 
 # @app.after_request
 # def after_request(response):
@@ -19,8 +23,13 @@ def index():
 #     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
 #     return response
 
+class HomepageGraph(Resource):
+    def get(self):
+        """Get a random homepage graph as json"""
+        graph = Graph()
+        return graph.as_json()
 
-# api.add_resource(WorkflowList, "/users/<user_id>/workflows")
+api.add_resource(HomepageGraph, "/homepage_graph")
 # api.add_resource(WorkflowItem, "/users/<user_id>/workflows/<workflow_id>")
 # api.add_resource(WorkflowStart, "/users/<user_id>/workflows/<workflow_id>/start")
 # api.add_resource(WorkflowInterrupt, "/users/<user_id>/workflows/<workflow_id>/interrupt")
