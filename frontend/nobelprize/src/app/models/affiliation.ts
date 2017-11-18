7d506d5a1ed6cc2cb13a4cc4bf75ded3b4b861c5
neteduandrei <andrei.netedu2009@gmail.com>
2017-11-18 02:13:19 +0000

    Add model for laureate json

---
+++ frontend/nobelprize/src/app/models/affiliation.ts
@@ -0,0 +1,5 @@
+export interface Affiliation{
+  city: String;
+  country: String;
+  name: String;
+}
--- frontend/nobelprize/src/app/services/laureates.service.ts
+++ frontend/nobelprize/src/app/services/laureates.service.ts
@@ -8,27 +8,11 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class LaureatesService {
-  laureate: string;
+  laureate: any;
 
   constructor(private http: HttpClient) { }
 
   getLaureateById(id: number): Observable<Laureate> {
-    this.http.get('http://localhost:5000/laureate/id/' + id).subscribe(response => {
-      this.laureate = response['results'];
-    });
-
-
-
-    const default_laureate = {name: 'andrei', description:
-      'The growth, division, and death of living cells are regulated by their genes. ' +
-      'If these functions are out of balance, tumors can form. One reason for this may be the incorporation of ' +
-      'virus genes into the genes of host cells. Harald zur Hausen demonstrated in 1983 that cervical cancer in ' +
-      'humans is caused by certain types of papilloma viruses (wart viruses), the genes from which are incorporated ' +
-      'into the host cells\' DNA. This discovery made it possible to develop a vaccine against cervical cancer, ' +
-      'which had been the second most common tumor disease in women.'};
-
-    return new Observable(observer => {
-      observer.next(default_laureate);
-    });
+    return this.http.get<Laureate>('http://localhost:5000/laureate/id/' + id);
   }
 }
