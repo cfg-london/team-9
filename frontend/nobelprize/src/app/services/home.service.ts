a25d05f4e5ce41b79d44a7da1b027710b34f897a
Stan Rares Alexandru <stan.rares@gmail.com>
2017-11-18 05:00:29 +0000

    Integrated with backend

--- frontend/nobelprize/src/app/services/home.service.ts
+++ frontend/nobelprize/src/app/services/home.service.ts
@@ -9,23 +9,15 @@ export class HomeService {
   constructor(private http: HttpClient) { }
 
   getConceptNodes(size: number): Observable<any[]> {
-    return new Observable((observer) => {
-      observer.next([
-        {
-          id: 1,
-          label: 'bla1',
-          size: 40,
-          type: 'laureate',
-          conceptId: '1'
-        },
-        {id: 2, label: 'bla2', size: 20, type: 'prize', conceptId: '1'}
-      ]);
-      observer.complete();
-  }).pipe(
-    map((resp: any[]) => resp.map(elem => {
-      elem.font = {size: elem.size};
-      return elem;
-    }))
-  );
+    return this.http.get(`http://localhost:5000/laureate/graph/id/1/limit/${size}`)
+        .pipe(
+          map((resp: any[]) => {
+            console.log(resp);
+            resp[0] = resp[0].map(elem => {
+              elem.font = {size: elem.size};
+              return elem;
+            });
+            return resp;
+          }));
   }
 }
